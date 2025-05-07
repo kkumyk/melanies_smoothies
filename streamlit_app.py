b6e9aafadc72c5fb2e0b046ee05f5301654fb262
Karina Kumyk <38696352+kkumyk@users.noreply.github.com>
2025-05-07 18:43:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,47 +21,29 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+
 if ingredients_list:
+
     ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    my_insert_stmt = f"""
-        insert into smoothies.public.orders(ingredients, name_on_order)
-        values ('{ingredients_string.strip()}', '{name_on_order}' )
-    """
+    # st.write(ingredients_string)
 
-    st.write(my_insert_stmt)  # Optional: useful for debugging
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """', '""" +name_on_order+ """' )"""
 
+    st.write(my_insert_stmt)
+    st.stop()
+    
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-# ingredients_list = st.multiselect(
-#     'Choose up to 5 ingredients:',
-#     my_dataframe,
-#     max_selections=5
-# )
-
-# if ingredients_list:
-
-#     ingredients_string = ''
-
-#     for fruit_chosen in ingredients_list:
-#         ingredients_string += fruit_chosen + ' '
-
-#     # st.write(ingredients_string)
-
-#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-#             values ('""" + ingredients_string + """', '""" +name_on_order+ """' )"""
-
-#     st.write(my_insert_stmt)
-#     st.stop()
-    
-#     time_to_insert = st.button('Submit Order')
-
-#     if time_to_insert:
-#         session.sql(my_insert_stmt).collect()
-#         st.success('Your Smoothie is ordered!', icon="✅")
